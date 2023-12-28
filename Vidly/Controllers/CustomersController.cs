e577fda31dbc23f79605bb5a28ca6080470b5592
Maros <mstruk20@gmail.com>
2023-12-28 15:47:58 +0100

    Edit customer

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -36,15 +36,44 @@ namespace Vidly.Controllers
         public IActionResult New()
         {
             var membershipTypes = _context.MembershipTypes.ToList();
-            var viewModel = new NewCustomerViewModel { MembershipTypes = membershipTypes };
+            var viewModel = new CustomerFormViewModel
+            {
+                Action = "New",
+                MembershipTypes = membershipTypes
+            };
 
-            return View(viewModel);
+            return View("CustomerForm", viewModel);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return NotFound();
+
+            var viewModel = new CustomerFormViewModel
+            {
+                Action = "Edit",
+                Id = id,
+                Name = customer.Name,
+                Birthdate = customer.Birthdate,
+                IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter,
+                MembershipTypeId = customer.MembershipTypeId,
+                MembershipTypes = _context.MembershipTypes.ToList(),
+            };
+
+            return View("CustomerForm", viewModel);
         }
 
         [HttpPost]
-        public IActionResult Create(Customer customer)
+        public IActionResult Save(Customer customer)
         {
-            _context.Customers.Add(customer);
+            if (customer.Id == 0)
+                _context.Customers.Add(customer);
+            else
+                _context.Customers.Update(customer);
+
             _context.SaveChanges();
 
             return RedirectToAction("Index", "Customers");
--- Vidly/ViewModels/CustomerFormViewModel.cs
+++ Vidly/ViewModels/CustomerFormViewModel.cs
@@ -3,8 +3,12 @@ using Vidly.Models;
 
 namespace Vidly.ViewModels
 {
-    public class NewCustomerViewModel
+    public class CustomerFormViewModel
     {
+        public string? Action { get; set; }
+
+        public int Id { get; set; }
+
         public string? Name { get; set; }
 
         [Display(Name = "Date of Birth")]
